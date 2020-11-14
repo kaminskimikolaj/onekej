eb98de40ac11d60588429cf1794a4063e62cf805
Mikołaj Kamiński <mikolajkaminski1@gmail.com>
2020-11-14 01:58:59 +0100

    change main naming

--- scripts/exportProduction.js
+++ scripts/exportProduction.js
@@ -1,4 +1,4 @@
 let fs = require('fs')
-let file = fs.readFileSync("interface.sandbox.js", "utf-8")
+let file = fs.readFileSync("interface.js", "utf-8")
 const regex = /test_seller/g;
-fs.writeFileSync("interface.production.js", file.replace(regex, "s2/login"))
+fs.writeFileSync("interface.js", file.replace(regex, "s2/login"))
--- scripts/exportSandbox.js
+++ scripts/exportSandbox.js
@@ -1,4 +1,4 @@
 let fs = require('fs')
-let file = fs.readFileSync("interface.production.js", "utf-8")
+let file = fs.readFileSync("interface.js", "utf-8")
 const regex = /s2\/login/g;
-fs.writeFileSync("interface.sandbox.js", file.replace(regex, "test_seller"))
+fs.writeFileSync("interface.js", file.replace(regex, "test_seller"))
